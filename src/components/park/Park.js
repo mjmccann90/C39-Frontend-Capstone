20afe7e1e5882b00836a0cb21943bf4fae7ebbc3
Mark McCann <mjmccann12@me.com>
2020-05-04 10:40:40 -0500

    added some logic to park

--- src/components/park/Park.js
+++ src/components/park/Park.js
@@ -2,9 +2,9 @@ import React from "react"
 
 export const dogPark = ({ dogPark }) => (
     <section className="dogPark">
-        <div className="dogPark__park_name"></div>
-        <div className="dogPark__address"></div>
-        <div className="dogPark__city"></div>
-        <div className="dogPark__state"></div>
+        <div className="dogPark__park_name">{dogPark.park_name}</div>
+        <div className="dogPark__street_address">{dogPark.street_address}</div>
+        <div className="dogPark__city">{dogPark.city}</div>
+        <div className="dogPark__state">{dogPark.state}</div>
     </section>
 )
