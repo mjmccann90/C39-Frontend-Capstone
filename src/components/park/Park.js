eb15d3851ef54b14addb42e53eddeee45842be52
Mark McCann <mjmccann12@me.com>
2020-05-04 13:19:06 -0500

    all the dog parks are displaying

--- src/components/park/Park.js
+++ src/components/park/Park.js
@@ -1,7 +1,7 @@
 import React from "react"
 // add css import
 
-export const dogPark = ({ dogPark }) => (
+export const DogPark = ({ dogPark }) => (
     <section className="dogPark">
         <div className="dogPark__park_name">{dogPark.park_name}</div>
         <div className="dogPark__street_address">{dogPark.street_address}</div>
