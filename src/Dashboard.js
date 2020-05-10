f4bce127f9486684c9ced83a986630f458582f38
Mark McCann <mjmccann12@me.com>
2020-05-10 11:22:14 -0500

    soft groupdemo

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,13 +2,13 @@ import React from "react"
 import { ParkDataProvider } from "../src/components/park/ParkDataProvider"
 import { ParkList } from "../src/components/park/ParkList"
 
-// import "./Layout.css"
+//import "./Layout.css"
 
 export const Dashboard = () => (
     <>
-        <h2>test</h2>
-        <small>test</small>
-        <h2>All the Bark Parks</h2>
+        <h1>615PupParks</h1>
+
+        <h2>All the Middle TN Dog Parks</h2>
         <ParkDataProvider>
             <ParkList />
         </ParkDataProvider>
